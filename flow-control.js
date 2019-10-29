79abe1c79da8b2ac762d1381c47b356e6075286d
Elmer Merejo <skenny135@gmail.com>
2019-10-29 15:16:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return 12 < age < 20 ?
+  return age >= 13 && age <= 19 ?
   "You are a teenager!" :
   "You are not a teenager"
 }
